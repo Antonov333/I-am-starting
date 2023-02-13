d98bfaa9a25f0877f2970a73e96dffd5d7f72055
Antonov333 <antonovsergei333@yahoo.com>
2023-02-13 14:55:23 +0300

    I am doing it again... ;)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! I am learning commits");
+        System.out.println("Hello world! I am learning commits. Although not all is clear yet, but I really like it :))");
     }
 }
